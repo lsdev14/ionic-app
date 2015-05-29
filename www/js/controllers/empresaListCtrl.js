b0ef07bddff2e6b467aace31f7abef2c131409b0
Leandro Souza <leandrolimpia@hotmail.com>
2015-05-28 22:44:20 -0300

    fixed pdf response

--- www/js/controllers/empresaListCtrl.js
+++ www/js/controllers/empresaListCtrl.js
@@ -63,6 +63,10 @@
         confirmPopup.then(function (res) {
             if (res) {
                 
+                $ionicLoading.show({
+                    template: 'Gerando relatório...'
+                });
+
                 console.log(JSON.stringify(empresa));
                 $http.post(
                     'http://apirelatorio.ssvsistemas.com.br/api/relatoriopresenca/post',
@@ -70,19 +74,41 @@
                     {
                         headers: {
                             'Content-Type': 'application/json'
-                        }
+                        },
+                        responseType:'arraybuffer'
                     }
                 ).success(function (data) {
-                    
-                    //console.log(data);
-                    console.log(data);
 
-                    cordova.plugins.email.open({
-                        to: empresa.email,
-                        subject: 'Relatório de presença - De: Até:',
-                        body: 'Em anexo segue arquivo com a lista de presença',
-                        attachments:"base64:relatorio.pdf//" + data
-                    });
+                    try {
+
+                        //console.log(data);
+                        console.log(true);
+                        $ionicLoading.hide();
+
+
+                        var file = new Blob([data],{ type: 'application/pdf' });
+                        var fileURL = URL.createObjectURL(file);
+
+                        //$scope.content = $sce.trustAsResourceUrl(fileURL);
+
+                        cordova.plugins.email.open({
+                            to: empresa.email,
+                            subject: 'Relatório de presença - De: Até:',
+                            body: 'Em anexo segue arquivo com a lista de presença',
+                            attachments: fileURL
+                        });
+                    }
+                    catch (ex) {
+
+                        var helpPopup = $ionicPopup.alert({
+                            title: 'Ajuda',
+                            template: ex
+                        });
+                        helpPopup.then(function (res) {
+                            console.log(res);
+                        });
+
+                    }
                 });
             }
         });
