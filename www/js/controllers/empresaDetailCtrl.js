67d705fd9f8f4ee47af96e9727b12596fd422fee
Leandro Souza <leandrolimpia@hotmail.com>
2015-08-18 23:41:27 -0300

    remover funcionario

--- www/js/controllers/empresaDetailCtrl.js
+++ www/js/controllers/empresaDetailCtrl.js
@@ -1,6 +1,6 @@
 ﻿angular.module('controlePresenca.controllers')
 
-.controller('EmpresaDetailCtrl',['$scope','$stateParams','$state','$ionicHistory','Empresas','Util',function ($scope,$stateParams,$state,$ionicHistory,empresas,util) {
+.controller('EmpresaDetailCtrl',['$scope','$ionicPopup','$stateParams','$state','$ionicHistory','Empresas','Util',function ($scope,$ionicPopup,$stateParams,$state,$ionicHistory,empresas,util) {
 
     $scope.form = {};
     $scope.edit = true;
@@ -43,6 +43,22 @@
 
         $scope.empresa.funcionarios.push(funcionario);
         $scope.funcionario.nome = "";
+    }
+
+    $scope.removeFuncionario = function (funcionario){
+        var confirmPopup = $ionicPopup.confirm({
+            title: 'Aviso',
+            template: 'Deseja remover o funcionário ' + funcionario.nome  + '?'
+        });
+        confirmPopup.then(function (res) {
+            if (res) {
+                console.log('Removendo funcionario:' + funcionario.nome);
+                $scope.empresa.funcionarios.splice($scope.empresa.funcionarios.indexOf(funcionario), 1);
+
+            } else {
+                console.log('Remover cancelado');
+            }
+        });
     }
 
 }]);
